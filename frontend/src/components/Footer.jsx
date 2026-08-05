3752965746a336d096bb540216b5444324df178b
webadmin <webadmin@wooden-ships.com>
2026-08-05 12:31:43 +0800

    edit footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,6 @@ export default function Footer() {
   return (
     <footer className="form-footer">
       <span>www.wooden-ships.com</span>
-      <span>woodenshipsknits.com</span>
       <span>@woodenshipsknits</span>
     </footer>
   )
